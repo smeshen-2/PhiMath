7979d19b59e79086a671dd8528015b7c386ef638
Kris <kristiyanmitkov25@gmail.com>
2023-11-18 22:35:26 +0200

    added UnsolvableException

---
+++ Datatypes/Exceptions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Exceptions;
+
+public class xException : Exception { }
+
+public class AxeQException : xException { public override string Message => "∀x∈ℚ"; }
+public class xeOException : xException { public override string Message => "x∈∅"; }
+public class UnsolvableException : xException { public override string Message => "Unsolvable"; }
--- Datatypes/Exceptions/xeOException.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace Exceptions;
-
-public class xeOException : Exception { }
